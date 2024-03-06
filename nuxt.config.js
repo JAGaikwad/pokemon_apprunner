56eddda12931a48a4698e574f1878fd3d080d215
JYOTIANIL GAIKWAD <e13721@kmjp.local>
2024-03-06 21:17:16 +0900

    6th March 2024 S3_buckets
    Signed-off-by: JYOTIANIL GAIKWAD <e13721@kmjp.local>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,12 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-//bucketName: "km-sd25-pokemon-ohio",
+
 export default defineNuxtConfig({
   css: ["animate.css"],
   runtimeConfig: {
     region: "us-east-1",
-    bucketName: "",
+    bucketName: "km-sd25-pokemon-ohio",
     public: {
       backendOrigin: undefined,
     },
